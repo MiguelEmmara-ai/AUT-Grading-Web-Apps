d6c378c50b0e3b08d231145913d52431a6516573
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2024-06-12 02:40:26 +1200

    Update Lock Files, Update Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
   return (
     <div className='navbar bg-base-100'>
       <div className='flex-1'>
-        <a className='btn btn-ghost text-xl'>AUT Grading</a>
+        <Link href='/' className='btn btn-ghost text-xl'>
+          AUT Grading
+        </Link>
       </div>
     </div>
   );
